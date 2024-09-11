26eace85a3ad6e6c30307ad4f5260deaa2ac6eb0
lwt227 <lwt0227@naver.com>
2024-09-11 15:37:13 +0900

    Add files via upload

---
+++ 28_3ProblemsWithFor/28.cpp
@@ -0,0 +1,34 @@
+﻿//1.별표를 n개를 출력한다.
+//2.1부터 n까지 홀수를 출력한다.
+//3.1부터 n까지 자연수를 모두 더한다.
+
+
+
+
+    
+#include <stdio.h>
+//for 문
+int main()
+{
+    int n;
+
+    printf("n을 입력하시오: ");
+    scanf_s("%d", &n);
+
+    //(1)
+    for (int i = 1; i <= n; i++)
+        printf("+");
+    printf("\n");
+
+    //(2)
+    for (int i = 1; i <= n; i++)
+        printf("%d ", i);
+    printf("\n");
+
+    //(3)
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("sum = %d\n", sum);
+}
